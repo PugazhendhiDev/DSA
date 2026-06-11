0accffe0b6f21741967fabc9e583ba44c134fc66
pugazhendhidev <pugazhendhiexplorer@gmail.com>
2026-06-11 21:22:24 +0530

    Two Pointers/boats_to_save_people

---
+++ two_pointers/boats_to_save_people/BoatsToSavePeople.java
@@ -0,0 +1,16 @@
+//LEETCODE 881. Boats to Save People
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0, right = people.length - 1, boat = 0;
+        while(left <= right) {
+            if(people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            boat++;
+        }
+        return boat;
+    }
+}
